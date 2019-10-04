48e6086a6d6bb7d59a547a2a1a136d70c7f47c3e
Jongheon Kim <sapzape@gmail.com>
2019-10-05 02:27:47 +0900

    Add simple CRUD service

--- src/main.controller.ts
+++ src/main.controller.ts
@@ -11,5 +11,11 @@ export class Controller {
 
   public routes() {
     this.app.route("/").get(this.donateService.helloMessage)
+    this.app.route("/donate").get(this.donateService.getAllDonate)
+    this.app.route("/donate").post(this.donateService.addNewDonate)
+    this.app
+      .route("/donate/:id")
+      .put(this.donateService.updateDonate)
+      .delete(this.donateService.deleteDonate)
   }
 }
--- src/services/donate.service.ts
+++ src/services/donate.service.ts
@@ -1,8 +1,43 @@
 import { Request, Response } from "express"
-import { HELLO_MESSAGE } from "../constants/donateApi.constants"
+import { MongooseDocument } from "mongoose"
 import { Donate } from "../models/donate.model"
+import { HELLO_MESSAGE } from "../constants/donateApi.constants"
+
 export class DonateService {
   public helloMessage(req: Request, res: Response) {
     return res.status(200).send(HELLO_MESSAGE)
+  }
+
+  public getAllDonate(req: Request, res: Response) {
+    Donate.find({}, (error: Error, donate: MongooseDocument) => {
+      if (error) res.send(error)
+      res.json(donate)
+    })
+  }
+
+  public addNewDonate(req: Request, res: Response) {
+    const newDonate = new Donate(req.body)
+    newDonate.save((error: Error, donate: MongooseDocument) => {
+      if (error) res.send(error)
+      res.json(donate)
+    })
+  }
+
+  public deleteDonate(req: Request, res: Response) {
+    const donateId = req.params.id
+    Donate.findByIdAndDelete(donateId, (error: Error, deleted: any) => {
+      if (error) res.send(error)
+      const message = deleted ? "Deleted successfully" : "Donate info not found"
+      res.send(message)
+    })
+  }
+
+  public updateDonate(req: Request, res: Response) {
+    const donateId = req.params.id
+    Donate.findByIdAndUpdate(donateId, req.body, (error: Error, donate: any) => {
+      if (error) res.send(error)
+      const message = donate ? "Updated successfully" : "Donate info not found"
+      res.send(message)
+    })
   }
 }
