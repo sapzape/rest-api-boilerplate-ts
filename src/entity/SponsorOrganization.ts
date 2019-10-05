a6cc1ee864d958b57ea352c165b4d31565ba45da
Jongheon Kim <sapzape@gmail.com>
2019-10-05 13:25:36 +0900

    Add Entity (Donate, SponsorOrganization)

---
+++ src/entity/SponsorOrganization.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column, Index, CreateDateColumn } from "typeorm"
+import { IsNotEmpty } from "class-validator"
+
+@Entity()
+@Index(["userId", "organizationId", "createdAt"], { unique: true })
+export class Donate {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  @IsNotEmpty()
+  userId: string
+
+  @Column()
+  @IsNotEmpty()
+  organizationId: number
+
+  @Column()
+  @IsNotEmpty()
+  amount: number
+
+  @Column()
+  @CreateDateColumn()
+  createdAt: Date
+}
