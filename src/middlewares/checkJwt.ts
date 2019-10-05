af587932eb80f52811c1c5d162b36a6dde082c62
Jongheon Kim <sapzape@gmail.com>
2019-10-05 12:47:50 +0900

    Add middleware

---
+++ src/middlewares/checkJwt.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express"
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "../constants/config.constants"
+
+export const checkJwt = (req: Request, res: Response, next: NextFunction) => {
+  const token = <string>req.headers["authorization"]
+  let jwtPayload
+
+  try {
+    jwtPayload = <any>jwt.verify(token, JWT_SECRET)
+    res.locals.jwtPayload = jwtPayload
+  } catch (error) {
+    return res.status(401).send(error)
+  }
+
+  const { userId, username } = jwtPayload
+  const newToken = jwt.sign({ userId, username }, JWT_SECRET, {
+    expiresIn: "1h"
+  })
+  res.setHeader("token", newToken)
+
+  return next()
+}
